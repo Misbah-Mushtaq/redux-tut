dd5da4de138c61f2053a473595b37ae61f2d3810
Misbah Mushtaq <misbahmushtaq75@gmail.com>
2021-10-21 14:36:19 +0500

    Reducer using map notation

--- redux-toolkit-prep/src/redux/slices/counterSlices.js
+++ redux-toolkit-prep/src/redux/slices/counterSlices.js
@@ -4,18 +4,32 @@ export const increment = createAction('increment/counter')
 export const decrement = createAction('decrement/counter')
 export const increaseAmount = createAction('increaseByAmount/counter')
 
-//reducers using builder notation
 const initialState = {
   value: 0
 }
-export const counterSlices = createReducer(initialState, builder => {
-  builder.addCase(increment, (state, action) => {
-      state.value++;
-  })
-  builder.addCase(decrement, (state, action) => {
-      state.value--;
-  })
-  builder.addCase(increaseAmount, (state, action) => {
-      state.value += action.payload;
-  })
+//reducers using builder notation
+
+// export const counterSlices = createReducer(initialState, builder => {
+//   builder.addCase(increment, (state, action) => {
+//       state.value++;
+//   })
+//   builder.addCase(decrement, (state, action) => {
+//       state.value--;
+//   })
+//   builder.addCase(increaseAmount, (state, action) => {
+//       state.value += action.payload;
+//   })
+// })
+
+//reducer using map notation
+export const counterSlices = createReducer(initialState, {
+  [increment]: (state, action)=>{
+    state.value++;
+  },
+  [decrement]: (state, action)=>{
+    state.value--;
+  },
+  [increaseAmount]: (state, action)=>{
+    state.value += action.payload;
+  }
 })
